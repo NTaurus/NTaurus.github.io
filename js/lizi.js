276d7091b89674aad480b5271538ae1dbd47c3ce
NTaurus <ntaurus@163.com>
2020-03-08 22:31:28 +0800

    updata

--- js/lizi.js
+++ js/lizi.js
@@ -1,54 +1,386 @@
-(function(){
-    window.Particle=function(container,params)
-    {params=params||{};
-    var options={point:300,minDis:100,mouseDis:200,effect:'zoom'};
-    var originParams={};
-    for(var param in params)
-    {if(typeof params[param]==='object')
-    {originParams[param]={};
-    for(var defPa in params[param])
-    {originParams[param][defPa]=params[param][defPa]}}
-    else
-    {originParams[param]=params[param]}};
-    for(var opt in options)
-    {if(typeof params[opt]==='object')
-    {for(var depOpt in options[opt])
-    {if(typeof params[opt][depOpt]==='undefined')
-    {params[opt][depOpt]=options[opt][depOpt]}}
-    }else if(typeof params[opt]==='undefined')
-    {params[opt]=options[opt]}};
-    var p=this;p.originParams=originParams;
-    p.params=params;
-    p.innerWidth=window.innerWidth;
-    p.innerHeight=window.innerHeight;
-    p.points=[];p.pageXY={pageX:p.innerWidth/2,pageY:p.innerHeight/2};
-    if(container[0]==='#'){p.container=container.split('#')[1]};
-    p.canvas=document.getElementById(p.container);
-    p.ctx=p.canvas.getContext('2d');
-    p.init=function(){p.canvas.width=p.innerWidth;
-    p.canvas.height=p.innerHeight;p.drawBackground();
-    p.addOverlap();p.initDrawPoint();p.animation();
-    p.mouseEvent();p.windowResize()};
-    p.addOverlap=function(){p.overlap=document.createElement('div');
-    p.overlap.style.width=p.innerWidth+'px';
-    p.overlap.style.height=p.innerHeight+'px';
-    p.overlap.style.position='absolute';
-    p.overlap.style.top='0px';
-    p.overlap.style.left='0px';
-    p.overlap.style.zIndex='100';
-    if(p.canvas.nextElementSibling){
-        p.canvas.parentNode.insertBefore(p.overlap,p.canvas.nextElementSibling)}
-        else{p.canvas.parentNode.appendChild(p.overlap)}};
-        p.color=function(){function random(){
-            return Math.round(Math.random()*255)};
-            this.r=random();this.g=random();
-            this.b=random();
-            this.a=random(1,0.8);
-            this.rgba='rgba('+this.r+','+this.g+','+this.b+','+this.a+')';
-            return this};
-            p.random=function(max,min){var min=arguments[1]||0;
-            return Math.floor(Math.random()*(max-min+1)+min)};
-            p.drawBackground=function(){if(!p.canvas){return}p.ctx.fillStyle='#000'
-            ;p.ctx.fillRect(0,0,p.innerWidth,p.innerHeight)};p.point=function(){this.color=new p.color();
-            this.x=Math.random()*p.innerWidth;this.y=Math.random()*p.innerHeight;this.xv=p.random(10,-10)/40;this.yv=p.random(10,-10)/40;this.r=p.random(3,1);this.scale=1};p.initDrawPoint=function(){for(var i=0;i<p.params.point;i+=1){var point=new p.point();p.points.push(point);p.ctx.beginPath();p.ctx.fillStyle=point.color.rgba;p.ctx.arc(point.x,point.y,point.r*point.scale,0,Math.PI*2,true);p.ctx.fill()};p.ctx.closePath()};p.connect=function(){function lineColor(p1,p2){var linear=p.ctx.createLinearGradient(p1.x,p1.y,p2.x,p2.y);linear.addColorStop(0,p1.color.rgba);linear.addColorStop(1,p2.color.rgba);return linear};for(var i=0;i<p.params.point;i+=1){for(var j=0;j<p.params.point;j+=1){var p1=p.points[i];var p2=p.points[j];if(Math.abs(p2.x-p1.x)<p.params.minDis&&Math.abs(p2.y-p1.y)<p.params.minDis){p.ctx.beginPath();p.ctx.lineWidth=0.2;p.ctx.strokeStyle=lineColor(p1,p2);p.ctx.moveTo(p1.x,p1.y);p.ctx.lineTo(p2.x,p2.y);p.ctx.stroke();p.ctx.closePath()}}}};p.lineto=function(){function isInView(point){return Math.abs(point.x-p.pageXY.pageX)<p.params.mouseDis&&Math.abs(point.y-p.pageXY.pageY)<p.params.mouseDis};(function line(){function lineColor(p1,p2){var linear=p.ctx.createLinearGradient(p1.x,p1.y,p2.x,p2.y);linear.addColorStop(0,p1.color.rgba);linear.addColorStop(1,p2.color.rgba);return linear};for(var i=0;i<p.params.point;i+=1){for(var j=0;j<p.params.point;j+=1){if(i!=j){var p1=p.points[i];var p2=p.points[j];if(isInView(p1)&&isInView(p2)){if(Math.abs(p2.x-p1.x)<p.params.minDis&&Math.abs(p2.y-p1.y)<p.params.minDis){p.ctx.beginPath();p.ctx.lineWidth=0.2;p.ctx.strokeStyle=lineColor(p1,p2);p.ctx.moveTo(p1.x,p1.y);p.ctx.lineTo(p2.x,p2.y);p.ctx.stroke();p.ctx.closePath()}}}}}})()};p.animation=function(){p.ctx.clearRect(0,0,p.innerWidth,p.innerHeight);p.drawBackground();for(var i=0;i<p.params.point;i+=1){var point=p.points[i];if(point.x<0||point.x>p.innerWidth){point.xv= -point.xv};if(point.y<0||point.y>p.innerHeight){point.yv= -point.yv};p.ctx.beginPath();p.ctx.fillStyle=point.color.rgba;point.x+=point.xv;point.y+=point.yv;p.ctx.arc(point.x,point.y,point.r*point.scale,0,Math.PI*2,true);p.ctx.fill()};if(p.params.effect=='zoom'){p.connect()}else if(p.params.effect=='line'){p.lineto()};requestAnimationFrame(p.animation)};p.mouseEvent=function(){p.overlap.addEventListener('mousemove',function(e){var e=e||window.event;var pageX=(e.clientX+document.body.scrollLeft||e.pageX)-this.offsetLeft;var pageY=(e.clientY+document.body.scrollTop||e.pageY)-this.offsetTop;if(p.params.effect=='zoom'){for(var t=0;t<p.params.point;t+=1){var point=p.points[t];if(Math.abs(point.x-p.pageXY.pageX)<p.params.minDis&&Math.abs(point.y-p.pageXY.pageY)<p.params.minDis){point.scale=5}else{point.scale=1}}};p.pageXY.pageX=pageX;p.pageXY.pageY=pageY});p.overlap.addEventListener('mouseout',function(e){if(p.params.effect=='zoom'){for(var i=0;i<p.params.point;i+=1){var point=p.points[i];if(point.scale!=1){point.scale=1}}}else{p.pageXY.pageX=p.innerWidth/2;p.pageXY.pageY=p.innerHeight/2}})};p.windowResize=function(){window.addEventListener('resize',p.init)};p.init();return p};window.requestAnimationFrame=(function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||function(callback)
-        {window.setTimeout(callback,1000/60)}})()})();
+/*通用模块依赖*/
+(function(window, factory){
+	
+	if(typeof define === "function" && define.amd){
+	//AMD
+		define(factory);
+	}else if(typeof module === "object" && module.exports){
+	//CMD
+		module.exports = factory();
+	}else{
+	//window
+		window.Dot = factory();
+	}
+
+}(typeof window !== "undefined" ? window : this, function(){
+	var Dot = function(selector, userOptions){
+		"use strict";
+
+		var options={
+			//canvas 默认宽高
+			cW: 1367,
+			cH: 500,
+
+			//粒子数目
+			numDot: 100,
+
+			//粒子半径
+			radDot: 15,
+
+			//大小是否不同
+			isRangeRad: true,
+
+			//粒子颜色
+			dotColor: "#FFFFFF",
+
+			//是否连线
+			isLine: true,
+
+			//线条长度
+			lineDist: 75,
+
+			//线条颜色
+			lineColor: "#FFFFFF",
+
+			//反弹系数
+			bounce: 1,
+
+			//透明度
+			opacity: 0.5,
+
+			//是否触摸连线
+			isTouch: false,
+
+			//x方向运动速度
+			vxRange: 2,
+
+			//y方向运动速度
+			vyRange: 2,
+
+			//是否与边界碰撞检测
+			isWallCollisionTest: true,
+
+			//球体间是否发生碰撞检测
+			isBallCollisionTest: true
+		};
+
+		/********
+		全局变量
+		********/
+		var oDots = [],
+			canvas = null,
+			ctx = null,
+			oDotA = null,
+			oDotB = null,
+			W = null, 
+			H = null;
+
+		/********
+		全局函数
+		********/
+
+		//1.参数混合
+		var mergeOptions = function(userOptions, options){
+			Object.keys(userOptions).forEach(function(key){
+				options[key] = userOptions[key];
+			});
+		}
+
+		//2.颜色16进制转RGBA
+		var colorToRGB = function(color, alpha){
+			if(typeof color === "string" && color[0] === "#"){
+				color = window.parseInt(color.slice(1), 16);
+			}
+			alpha = (alpha === undefined) ? 1 : alpha;
+
+			//解析
+			var r = color >> 16 & 0xff,
+				g = color >> 8 & 0xff,
+				b = color >> 4 & 0xff,
+				a = (alpha < 0 ) ? 0 : ((alpha > 1) ? 1 : alpha);
+
+			if (a === 1) {
+    			return "rgb("+ r +","+ g +","+ b +")";
+  			} else {
+    			return "rgba("+ r +","+ g +","+ b +","+ a +")";
+  			}
+		}
+
+		//3.鼠标捕获函数
+		var captureMouse = function(element){
+			var mouse = {x: 0,y: 0};
+
+			element.addEventListener('mousemove', function(event){
+            var x,y;
+            if(event.pageX||event.pageY){
+                x = event.pageX;
+                y = event.pageY;
+            }else{
+                x = event.clientX + document.body.scrollLeft +document.documentElement.scrollLeft;
+                y = event.clientY + document.body.scrollTop +document.documentElement.scrollTop;
+            }
+            
+            x -= element.offsetLeft;
+            y -= element.offsetTop;
+            
+            mouse.x = x;
+            mouse.y = y;   
+
+            //console.log(mouse.x, mouse.y);
+
+           },false);
+         
+         return mouse;  
+		}
+
+		//4.范围函数
+		var ran = function(min, max){
+			var val = max - min;
+			return parseInt((Math.random()*val + min).toFixed(2));
+		}
+
+		//5.粒子函数
+		var Practicle = function(color, radius, alpha){
+    		this.x = 0;
+    		this.y = 0;
+    		this.vx = 0;
+    		this.vy = 0;
+    		this.radius = radius;
+    		this.rotation = 0;
+    		this.mass = 1;
+    		this.scaleX = 1;
+    		this.scaleY = 1;
+    		this.name = "";
+    		this.color = colorToRGB(color, alpha);
+		}
+		Practicle.prototype.draw = function(ctx){
+			ctx.save();
+    		ctx.translate(this.x,this.y);
+    		ctx.rotate(this.rotation);
+    		ctx.scale(this.scaleX,this.scaleY);
+    		ctx.fillStyle = this.color;
+    		ctx.strokeStyle = this.color;
+    		ctx.beginPath();
+    		ctx.arc(0,0,this.radius,0,Math.PI*2,false);
+    		ctx.closePath();
+    		ctx.fill();
+    		ctx.restore();
+		}
+
+		//6.make 粒子
+		var makeDots = function(){
+			for(var i=0; i<options.numDot; i++){
+					if(options.isRangeRad){
+						options.radDot = Math.floor(ran(2, options.radDot));
+					}
+					var oDot = new Practicle(options.dotColor, options.radDot, options.opacity);
+					    oDot.x = ran(0, W);
+					    oDot.y = ran(0, H);
+					    oDot.vx = Math.random()*(options.vxRange) - options.vxRange/2;
+					    oDot.vy = Math.random()*(options.vyRange) - options.vyRange/2;
+					    oDots.push(oDot);
+				}
+		}
+
+		//7. 绘制粒子
+		var draw = function(oDot){
+				oDot.draw(ctx);
+		}
+
+		//8. 运动函数
+		var move = function(oDot){
+			oDot.x += oDot.vx;
+			oDot.y += oDot.vy;
+
+			checkWall(oDot);
+
+		}
+
+		//9.墙壁碰撞检测
+		var checkWall = function(oDot){
+			if(options.isWallCollisionTest){
+					if(oDot.x + oDot.radius > W){
+						oDot.x = W - oDot.radius;
+						oDot.vx *= -(options.bounce);
+					}else if(oDot.x - oDot.radius < 0){
+						oDot.x = oDot.radius;
+						oDot.vx *= -(options.bounce);
+					}
+
+					if(oDot.y + oDot.radius > H){
+						oDot.y = H - oDot.radius;
+						oDot.vy *= -(options.bounce);
+					}else if(oDot.y - oDot.radius < 0){
+						oDot.y = oDot.radius;
+						oDot.vy *= -(options.bounce);
+					}
+			}else{
+
+					if(oDot.x - oDot.radius > W){
+						oDot.x = 0;
+					}else if(oDot.x + oDot.radius < 0){
+						oDot.x = W;
+					}
+
+					if(oDot.y - oDot.radius > H){
+						oDot.y = 0;
+					}else if(oDot.y + oDot.radius < 0){
+						oDot.y = H;
+					}
+			}
+		}
+
+		//10.旋转
+		var rotate = function(x, y, sin, cos, reverse){
+			 return {
+                   x: (reverse)?(x*cos + y*sin):(x*cos - y*sin),
+                   y: (reverse)?(y*cos - x*sin):(y*cos + x*sin)
+               }
+		}
+
+		//11.球体碰撞检测
+		var checkCollision = function(oDot0, oDot1){
+               var dx = oDot1.x - oDot0.x,
+                   dy = oDot1.y - oDot0.y,
+                   dist = Math.sqrt(dx*dx + dy*dy);
+               
+               if(dist < oDot0.radius + oDot1.radius){
+                   var angle = Math.atan2(dy, dx),
+                       sin = Math.sin(angle),
+                       cos = Math.cos(angle);
+                   
+                   //rotate oDot0 position
+                   var pos0 = {
+                       x: 0,
+                       y: 0 
+                   }
+                   
+                   //rotate oDot1 position
+                   var pos1 = rotate(dx, dy, sin, cos, true);
+                   
+                   //rotate oDot0 velocity
+                   var vel0 = rotate(oDot0.vx, oDot0.vy, sin, cos, true);
+                   
+                   //rotate oDot1 velcoity
+                   var vel1 = rotate(oDot1.vx, oDot1.vy, sin, cos, true);
+                   
+                   //collision reaction
+                   
+                   var vxTotal = vel0.x - vel1.x;
+                   
+                   vel0.x = ((oDot0.mass - oDot1.mass)*vel0.x + 2*oDot1.mass*vel1.x)/(oDot0.mass + oDot1.mass);
+                   vel1.x = vxTotal + vel0.x;
+                   
+                   //update position - to avoid objects becoming stuck together
+                   var absV = Math.abs(vel0.x) + Math.abs(vel1.x),
+                       overlap = (oDot0.radius + oDot1.radius) - Math.abs(pos0.x - pos1.x);
+                   //update position
+                   pos0.x += vel0.x / absV * overlap;
+                   pos1.x += vel1.x / absV * overlap;
+                   
+                   //rotate everything back
+                   var pos0F = rotate(pos0.x, pos0.y, sin, cos, false);
+                   var pos1F = rotate(pos1.x, pos1.y, sin, cos, false);
+                   
+                   //adjust position to actual screen position
+                   oDot1.x = oDot0.x + pos1F.x;
+                   oDot1.y = oDot0.y + pos1F.y;
+                   oDot0.x = oDot0.x + pos0F.x;
+                   oDot0.y = oDot0.y + pos0F.y;
+                   
+                   //rotate velocity back
+                   var vel0F = rotate(vel0.x, vel0.y, sin, cos, false);
+                   var vel1F = rotate(vel1.x, vel1.y, sin, cos, false);
+                   
+                   oDot0.vx = vel0F.x;
+                   oDot0.vy = vel0F.y;
+                   oDot1.vx = vel1F.x;
+                   oDot1.vy = vel1F.y;
+                   
+               }
+           }
+
+		//12.连线
+		var drawLine = function(oDot0, oDot1){
+			var dx = oDot1.x - oDot0.x,
+				dy = oDot1.y - oDot0.y,
+				dist = Math.sqrt(dx*dx + dy*dy);
+
+				if(dist < options.lineDist){
+				   ctx.save();
+                   ctx.strokeStyle = colorToRGB(options.lineColor, (options.opacity-0.4) <= 0 ? 0.1 : options.opacity-0.4);
+                   ctx.beginPath();
+                   ctx.moveTo(oDot0.x, oDot0.y);
+                   ctx.lineTo(oDot1.x, oDot1.y);
+                   ctx.closePath();
+                   ctx.stroke();
+                   ctx.restore();
+				}
+
+		}
+
+		//13.执行
+		var execute = function(){
+			oDots.forEach(move);
+
+			for(var i=0; i<options.numDot-1; i++){
+					oDotA = oDots[i];
+					for(var j=i+1; j<options.numDot; j++){
+						oDotB = oDots[j];
+						options.isBallCollisionTest&&checkCollision(oDotA, oDotB);
+						options.isLine&&drawLine(oDotA, oDotB);
+					}
+			}
+
+			oDots.forEach(draw);
+		}
+        
+        //14.动画兼容
+        if(!window.requestAnimationFrame){
+            window.requestAnimationFrame =(window.webkitRequestAnimationFrame||
+                                           window.mozRequestAnimationFrame||
+                                           window.oRequestAnimationFrame||
+                                           window.msRequestAnimationFrame||
+                                           function(callback){
+                                                return window.setTimeout(callback,1000/60); 
+                                           });
+        }
+
+
+		/********
+		初始化
+		********/
+
+		var initDot = function(selector, userOptions){
+
+			//混合参数
+			mergeOptions(userOptions, options);
+
+			canvas = document.getElementById(selector),
+			ctx = canvas.getContext('2d');
+            
+            W = options.cW;
+			H = options.cH;
+				
+			canvas.width = W;
+			canvas.height = H;
+
+			//渲染粒子
+			makeDots();
+
+			(function drawFrame(){
+				window.requestAnimationFrame(drawFrame);
+				ctx.clearRect(0, 0, W, H);
+
+				execute();
+
+			}())
+
+		}
+
+		initDot(selector, userOptions);
+
+	}
+
+	return Dot;
+}))
