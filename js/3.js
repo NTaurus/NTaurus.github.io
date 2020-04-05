b52c0979b6c058817e7daafad625df22d715f421
NTaurus <ntaurus@163.com>
2020-04-05 22:42:10 +0800

    updata

--- js/3.js
+++ js/3.js
@@ -1,207 +1,140 @@
-(function ($) {
-	var weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+/*
+*名称：网站烟花点击效果
+*作者：ONESRC
+*博客：https://www.onesrc.cn/
+*来源：https://www.onesrc.cn/p/add-a-fireworks-click-effect-to-your-website.html
+*/
+function clickEffect() {
+    let balls = [];
+    let longPressed = false;
+    let longPress;
+    let multiplier = 0;
+    let width, height;
+    let origin;
+    let normal;
+    let ctx;
+    const colours = ["#F73859", "#14FFEC", "#00E0FF", "#FF99FE", "#FAF15D"];
+    const canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+    canvas.setAttribute("style", "width: 100%; height: 100%; top: 0; left: 0; z-index: 99999; position: fixed; pointer-events: none;");
+    const pointer = document.createElement("span");
+    pointer.classList.add("pointer");
+    document.body.appendChild(pointer);
+
+    if (canvas.getContext && window.addEventListener) {
+        ctx = canvas.getContext("2d");
+        updateSize();
+        window.addEventListener('resize', updateSize, false);
+        loop();
+        window.addEventListener("mousedown", function(e) {
+            pushBalls(randBetween(10, 20), e.clientX, e.clientY);
+            document.body.classList.add("is-pressed");
+            longPress = setTimeout(function() {
+                document.body.classList.add("is-longpress");
+                longPressed = true;
+            }, 500);
+        }, false);
+        window.addEventListener("mouseup", function(e) {
+            clearInterval(longPress);
+            if (longPressed == true) {
+                document.body.classList.remove("is-longpress");
+                pushBalls(randBetween(50 + Math.ceil(multiplier), 100 + Math.ceil(multiplier)), e.clientX, e.clientY);
+                longPressed = false;
+            }
+            document.body.classList.remove("is-pressed");
+        }, false);
+        window.addEventListener("mousemove", function(e) {
+            let x = e.clientX;
+            let y = e.clientY;
+            pointer.style.top = y + "px";
+            pointer.style.left = x + "px";
+        }, false);
+    } else {
+        console.log("canvas or addEventListener is unsupported!");
+    }
 
 
-	function AvailabilityCalendar(container, bookedDates) {
-		this.date = new Date();
-		this.date.setDate(1);
+    function updateSize() {
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.width = window.innerWidth + 'px';
+        canvas.style.height = window.innerHeight + 'px';
+        ctx.scale(2, 2);
+        width = (canvas.width = window.innerWidth);
+        height = (canvas.height = window.innerHeight);
+        origin = {
+            x: width / 2,
+            y: height / 2
+        };
+        normal = {
+            x: width / 2,
+            y: height / 2
+        };
+    }
+    class Ball {
+        constructor(x = origin.x, y = origin.y) {
+            this.x = x;
+            this.y = y;
+            this.angle = Math.PI * 2 * Math.random();
+            if (longPressed == true) {
+                this.multiplier = randBetween(14 + multiplier, 15 + multiplier);
+            } else {
+                this.multiplier = randBetween(6, 12);
+            }
+            this.vx = (this.multiplier + Math.random() * 0.5) * Math.cos(this.angle);
+            this.vy = (this.multiplier + Math.random() * 0.5) * Math.sin(this.angle);
+            this.r = randBetween(8, 12) + 3 * Math.random();
+            this.color = colours[Math.floor(Math.random() * colours.length)];
+        }
+        update() {
+            this.x += this.vx - normal.x;
+            this.y += this.vy - normal.y;
+            normal.x = -2 / window.innerWidth * Math.sin(this.angle);
+            normal.y = -2 / window.innerHeight * Math.cos(this.angle);
+            this.r -= 0.3;
+            this.vx *= 0.9;
+            this.vy *= 0.9;
+        }
+    }
 
-		this.container = container;
-		this.bookedDates = bookedDates;
+    function pushBalls(count = 1, x = origin.x, y = origin.y) {
+        for (let i = 0; i < count; i++) {
+            balls.push(new Ball(x, y));
+        }
+    }
 
-		this.createCalendar();
-		this.renderMonth();
-	}
+    function randBetween(min, max) {
+        return Math.floor(Math.random() * max) + min;
+    }
 
+    function loop() {
+        ctx.fillStyle = "rgba(255, 255, 255, 0)";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < balls.length; i++) {
+            let b = balls[i];
+            if (b.r < 0) continue;
+            ctx.fillStyle = b.color;
+            ctx.beginPath();
+            ctx.arc(b.x, b.y, b.r, 0, Math.PI * 2, false);
+            ctx.fill();
+            b.update();
+        }
+        if (longPressed == true) {
+            multiplier += 0.2;
+        } else if (!longPressed && multiplier >= 0) {
+            multiplier -= 0.4;
+        }
+        removeBall();
+        requestAnimationFrame(loop);
+    }
 
-	AvailabilityCalendar.prototype = {
-		/**
-		 * Setup methods
-		 */
-		__createToolbar: function () {
-			var $toolbar = $('<div></div>').appendTo(this.container);
-			$toolbar.addClass('availability-calendar-toolbar');
-
-			this.$monthLabel = $('<span></span>').appendTo($toolbar);
-			var $inputContainer = $('<span></span>').appendTo($toolbar);
-
-			$inputContainer.append('<input type="button" title="This month" value="This Month">');
-			$inputContainer.append('<input type="button" title="Previous month" value="&#10094;">');
-			$inputContainer.append('<input type="button" title="Next month" value="&#10095;">');
-
-			var $inputs = $inputContainer.children('input');
-			var self = this;
-
-			$inputs.eq(0).on('click', function () {
-				self.date = new Date();
-				self.date.setDate(1);
-				self.renderMonth();
-			});
-
-			$inputs.eq(1).on('click', function () {
-				self.date.setMonth(self.date.getMonth() - 1);
-				self.renderMonth();
-			});
-
-			$inputs.eq(2).on('click', function () {
-				self.date.setMonth(self.date.getMonth() + 1);
-				self.renderMonth();
-			});
-		},
-		__createTable: function () {
-			var $table = $('<table></table>').appendTo(this.container);
-			$table.addClass('availability-calendar');
-
-			// Weekday headers
-			var $tr = $('<tr class="days"></tr>').appendTo($table);
-
-			weekdays.forEach(function (day) {
-				$('<th></th>').html(day).appendTo($tr);
-			});
-
-			// Day cells
-			for (var i = 0; i < 6; ++i) {
-				$tr = $('<tr class="date"></tr>').appendTo($table);
-				$tr.append('<td></td><td></td><td></td><td></td><td></td><td></td><td></td>');
-			}
-
-			this.$cells = $table.find('td');
-		},
-		createCalendar: function () {
-			this.__createToolbar();
-			this.__createTable();
-		},
-
-
-		/**
-		 * Month rendering methods
-		 */
-		__addPreviousMonthDays: function (date, cellIndexes, dates) {
-			var firstWeekdayOfMonth = date.getDay() - 1;
-			if (firstWeekdayOfMonth < 0) firstWeekdayOfMonth = 6;
-
-			if (firstWeekdayOfMonth > 0) {
-				date.setDate(0);
-				var numDays = date.getDate();
-
-				for (var i = numDays - firstWeekdayOfMonth + 1; i <= numDays; ++i) {
-					this.$cells.eq(dates.length).html(i).addClass('ex-month');
-
-					date.setDate(i);
-					var dateInt = date.valueOf();
-
-					cellIndexes[dateInt] = dates.length;
-					dates.push(dateInt);
-				}
-			}
-		},
-		__addThisMonthDays: function (date, year, month, cellIndexes, dates) {
-			date.setFullYear(year, month + 1, 0); // Need to reset year
-			var numDays = date.getDate();
-
-			for (var i = 1; i <= numDays; ++i) {
-				this.$cells.eq(dates.length).html(i);
-
-				date.setDate(i);
-				var dateInt = date.valueOf();
-
-				cellIndexes[dateInt] = dates.length;
-				dates.push(dateInt);
-			}
-		},
-		__addNextMonthDays: function (date, month, cellIndexes, dates) {
-			if (dates.length < 42) {
-				date.setMonth(month + 1, 1);
-				var remainingDays = 42 - dates.length;
-
-				for (var i = 1; i <= remainingDays; ++i) {
-					this.$cells.eq(dates.length).html(i).addClass('ex-month');
-
-					date.setDate(i);
-					var dateInt = date.valueOf();
-
-					cellIndexes[dateInt] = dates.length;
-					dates.push(dateInt);
-				}
-			}
-		},
-		__addEvents: function (cellIndexes, dates) {
-			var firstDate = dates[0];
-			var lastDate = dates[dates.length - 1];
-			var self = this;
-
-			this.bookedDates.forEach(function (date) {
-				if (date.start <= lastDate && date.end >= firstDate) {
-					var startIndex = cellIndexes[date.start];
-					var endIndex = cellIndexes[date.end];
-
-					if (startIndex !== undefined) {
-						self.$cells.eq(startIndex).addClass('unavailable').append('<div class="first"></div>');
-						++startIndex;
-					}
-					else {
-						startIndex = cellIndexes[firstDate];
-					}
-
-					if (endIndex !== undefined) {
-						self.$cells.eq(endIndex).addClass('unavailable').append('<div class="last"></div>');
-						--endIndex;
-					}
-					else {
-						endIndex = cellIndexes[lastDate];
-					}
-
-					self.$cells.slice(startIndex, endIndex + 1).addClass('unavailable').append('<div></div>');
-				}
-			});
-		},
-		renderMonth: function () {
-			var cellIndexes = {};
-			var dates = [];
-
-			var year = this.date.getFullYear();
-			var month = this.date.getMonth();
-			var date = new Date(year, month, 1);
-
-			this.$monthLabel.html(months[month] + ' ' + year);
-			this.$cells.removeClass('ex-month');
-			this.$cells.filter('.unavailable').removeClass('unavailable').children().remove();
-
-			this.__addPreviousMonthDays(date, cellIndexes, dates);
-			this.__addThisMonthDays(date, year, month, cellIndexes, dates);
-			this.__addNextMonthDays(date, month, cellIndexes, dates);
-
-			this.__addEvents(cellIndexes, dates);
-		}
-	};
-
-
-	$.fn.availabilityCalendar = function (bookedDates) {
-		var dates = [];
-
-		bookedDates.forEach(function (date) {
-			var start = new Date(date.start);
-			var end = new Date(date.end);
-
-			start.setHours(0, 0, 0, 0);
-			end.setHours(0, 0, 0, 0);
-
-			start = start.valueOf();
-			end = end.valueOf();
-
-			if (start <= end) {
-				dates.push({
-					start: start,
-					end: end
-				});
-			}
-		});
-
-		this.each(function () {
-			new AvailabilityCalendar(this, dates);
-		});
-
-		return this;
-	};
-})(jQuery);
+    function removeBall() {
+        for (let i = 0; i < balls.length; i++) {
+            let b = balls[i];
+            if (b.x + b.r < 0 || b.x - b.r > width || b.y + b.r < 0 || b.y - b.r > height || b.r < 0) {
+                balls.splice(i, 1);
+            }
+        }
+    }
+}
+clickEffect();//调用
